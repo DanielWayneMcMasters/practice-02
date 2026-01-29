5ad61f63a3b2bc59c1ab452d25a3853ee31c4be6
K3N-s <kkey2@ualr.edu>
2026-01-29 14:41:52 +0000

    Finish project

---
+++ program2.py
@@ -0,0 +1,9 @@
+# AI Disclaimer: This code was written without the use of AI tools.
+
+temp_C = float(input("Enter temperature in Celsius: "))
+
+temp_F = (temp_C * 9/5) + 32
+temp_K = temp_C + 273.15
+
+print(f"{temp_C:.1f}°C = {temp_F:.1f}°F = {temp_K:.1f}K")
+

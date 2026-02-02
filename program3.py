89429955c00f50f5ba03510966ea160b591666c1
DanielWayneMcMasters <dwmcmaster1@ualr.edu>
2026-02-02 17:06:10 +0000

    Program 3

--- program3.py
+++ program3.py
@@ -1,4 +1,17 @@
+# Nicholas Seward
+# Simple Calculator
 # AI Disclaimer: This code was written without the use of AI tools.
+
+# Enter first number: 14
+# Enter second number: 8
+# ==========================================
+# Calculation Results
+# ==========================================
+# 14.00 + 8.00 = 22.00
+# 14.00 - 8.00 = 6.00
+# 14.00 * 8.00 = 112.00
+# 14.00 / 8.00 = 1.75
+# ==========================================
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
